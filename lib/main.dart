fe3ba03b9f0237d09eadbcaf8a496fd414444f12
BuffDoge <153471156+TRZs69@users.noreply.github.com>
2026-02-25 17:11:49 +0700

    fix(mobile): initialize Supabase to resolve quick access screen crash

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,16 @@ import 'package:app/view/login_screen.dart';
 import 'package:app/view/main_screen.dart';
 import 'package:app/view/onboarding_screen.dart';
 
-void main() {
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await Supabase.initialize(
+    url: 'https://itarozdimxukkhwxruti.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2aXZmcW5xeG5wZnBpanJ2a2tiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MTQxMjEsImV4cCI6MjA3NDE5MDEyMX0.VwNktSJnyCuvBHEEMw4hv4wsHm7wT1MxS6foqR2i4Nk',
+  );
+
   runApp(const LevelyApp());
 }
 
